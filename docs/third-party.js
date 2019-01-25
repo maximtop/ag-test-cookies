3b3d543b416784cd784e441f1624d5d198a5ac6d
Maxim Topchu <maximtop@gmail.com>
2019-01-25 12:44:59 +0300

    fix

--- docs/third-party.js
+++ docs/third-party.js
@@ -1,6 +1,9 @@
-console.log('Hi from the third-party script');
+; function () {
+    console.log('Hi from the third-party script');
 
-const date = Date.now() + 1000 * 60 * 60 * 24 * 30;
-const dateString = new Date(date).toUTCString();
-document.cookie = `userName=maxim; path=/; domain=localhost; expires=${dateString}`;
-document.cookie = 'userName=vasiya';
+    const date = Date.now() + 1000 * 60 * 60 * 24 * 30;
+    const dateString = new Date(date).toUTCString();
+    document.cookie = `userName=maxim; path=/; domain=localhost; expires=${dateString}`;
+    document.cookie = 'userName=vasiya';
+}
+
--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
-console.log('Hi from the first-party script');
+; fucnction () {
+    console.log('Hi from the first-party script');
 
-const date = Date.now() + 1000 * 60 * 60 * 24 * 30;
-const dateString = new Date(date).toUTCString();
-document.cookie = `userName=maxim; path=/; domain=facebook.com; expires=${dateString}`;
+    const date = Date.now() + 1000 * 60 * 60 * 24 * 30;
+    const dateString = new Date(date).toUTCString();
+    document.cookie = `userName=maxim; path=/; domain=facebook.com; expires=${dateString}`;
+}();
+
