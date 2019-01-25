54ead5b39797676a66460c14113f0b7898498cfe
Maxim Topchu <maximtop@gmail.com>
2019-01-25 12:55:18 +0300

    fix

--- docs/third-party.js
+++ docs/third-party.js
@@ -3,6 +3,6 @@
 
     const date = Date.now() + 1000 * 60 * 60 * 24 * 30;
     const dateString = new Date(date).toUTCString();
-    document.cookie = `script=third-party; path=/; domain=github.com; expires=${dateString}`;
+    document.cookie = `script=third-party; path=/; domain=github.io; expires=${dateString}`;
 })()
 
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@
 
     const date = Date.now() + 1000 * 60 * 60 * 24 * 30;
     const dateString = new Date(date).toUTCString();
-    document.cookie = `script=first-party; path=/; domain=facebook.com; expires=${dateString}`;
+    document.cookie = `script=first-party; path=/; expires=${dateString}`;
+    document.cookie = `other=first-party; path=/; expires=${dateString}`;
 })();
 
