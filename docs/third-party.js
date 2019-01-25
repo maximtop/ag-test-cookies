c70dc71b0ef3e4c8e89e2cca055a61bcbd20a6fb
Maxim Topchu <maximtop@gmail.com>
2019-01-25 13:00:10 +0300

    fix

--- docs/third-party.js
+++ docs/third-party.js
@@ -3,6 +3,9 @@
 
     const date = Date.now() + 1000 * 60 * 60 * 24 * 30;
     const dateString = new Date(date).toUTCString();
-    document.cookie = `script=third-party; path=/; domain=github.io; expires=${dateString}`;
+    document.cookie = `third-party-domain-github.io=github.io; path=/; domain=github.io; expires=${dateString}`;
+    document.cookie = `third-party-no-domain=no-domain; path=/; expires=${dateString}`;
+    document.cookie = `third-party-domain-localhost=localhost; path=/; domain=localhost; expires=${dateString}`;
+    document.cookie = `third-party-domain-maximtop.github.io=maximtop.github.io; domain=maximtop.github.io; path=/; expires=${dateString}`;
 })()
 
--- src/index.js
+++ src/index.js
@@ -3,7 +3,9 @@
 
     const date = Date.now() + 1000 * 60 * 60 * 24 * 30;
     const dateString = new Date(date).toUTCString();
-    document.cookie = `script=first-party; path=/; expires=${dateString}`;
-    document.cookie = `other=first-party; path=/; expires=${dateString}`;
+    document.cookie = `first-party-no-domain=no-domain; path=/; expires=${dateString}`;
+    document.cookie = `first-party-domain-localhost=localhost; domain=localhost; path=/; expires=${dateString}`;
+    document.cookie = `first-party-domain-github.io=github.io; domain=github.io; path=/; expires=${dateString}`;
+    document.cookie = `first-party-domain-maximtop.github.io=maximtop.github.io; domain=maximtop.github.io; path=/; expires=${dateString}`;
 })();
 
