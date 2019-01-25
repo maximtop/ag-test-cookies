42de6acfbec46c8cd3be76bd6ee9fe1822636244
Maxim Topchu <maximtop@gmail.com>
2019-01-25 12:46:55 +0300

    fix

--- docs/third-party.js
+++ docs/third-party.js
@@ -1,9 +1,9 @@
-; function () {
+; (function () {
     console.log('Hi from the third-party script');
 
     const date = Date.now() + 1000 * 60 * 60 * 24 * 30;
     const dateString = new Date(date).toUTCString();
     document.cookie = `userName=maxim; path=/; domain=localhost; expires=${dateString}`;
     document.cookie = 'userName=vasiya';
-}
+})()
 
--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
-; fucnction () {
+;(function () {
     console.log('Hi from the first-party script');
 
     const date = Date.now() + 1000 * 60 * 60 * 24 * 30;
     const dateString = new Date(date).toUTCString();
     document.cookie = `userName=maxim; path=/; domain=facebook.com; expires=${dateString}`;
-}();
+})();
 
